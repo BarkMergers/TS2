cad7cbe92a6cbdc16188ee348fc9ba8f35a65be4
Mark Burgess <mburgess1983@gmail.com>
2025-08-13 22:38:39 +0100

    test 99

--- reactproject5/vite.config.ts
+++ reactproject5/vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [tailwindcss(), plugin()],
-
     base: './', // use relative paths
     server: {
         port: 56394,
