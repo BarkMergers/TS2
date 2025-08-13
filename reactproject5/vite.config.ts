1b2638b1517c8a65fde7f789079df507c9adc60d
Mark Burgess <mburgess1983@gmail.com>
2025-08-13 19:04:34 +0100

    init c

--- reactproject5/vite.config.ts
+++ reactproject5/vite.config.ts
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [tailwindcss(), plugin()],
+
+    base: './', // use relative paths
     server: {
         port: 56394,
     }
